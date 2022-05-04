814517d145d804b0e45269fb634727fd7150e364
sanurag <sanurag@shoprite.co.za>
2022-05-04 20:49:29 +0200

    First Commit

--- Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
@@ -5,7 +5,25 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetLongestSentance(string s)
         {
-            throw new NotImplementedException();
+            char[] specialChar = { '.', '?' };
+            string[] sentece = s.Split(specialChar);
+            int lenght = sentece.GetLength();
+            int maxCount = 0;
+
+            for (int i = lenght - 1; i >= 0; i--)
+            {
+                string[] words = sentece[i].Split(' ');
+                int wordCount = words.GetLength();
+                if (maxCount < wordCount)
+                {
+                    maxCount = wordCount;
+                }
+            }
+
+            return maxCount;
+
+
+            // throw new NotImplementedException();
         }
     }
 }
